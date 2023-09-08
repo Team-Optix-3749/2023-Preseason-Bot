af486c5642a6913bcca2fbe2743bd3095f5da71d
Harkirat47 <har.ki.kirat@gmail.com>
2023-09-07 19:47:27 -0700

    Its clean now

--- src/main/java/frc/robot/subsystems/tankdrive.java
+++ src/main/java/frc/robot/subsystems/tankdrive.java
@@ -41,29 +41,6 @@ public class tankdrive extends SubsystemBase {
     public void stop() {
     differentialDrive.arcadeDrive(0, 0);
   }
-  /**
-   * Example command factory method.
-   *
-   * @return a command
-   */
-  // public CommandBase exampleMethodCommand() {
-  //   // Inline construction of command goes here.
-  //   // Subsystem::RunOnce implicitly requires `this` subsystem.
-  //   return runOnce(
-  //       () -> {
-  //         /* one-time action goes here */
-  //       });
-  // }
-
-  /**
-   * An example method querying a boolean state of the subsystem (for example, a digital sensor).
-   *
-   * @return value of some boolean subsystem state, such as a digital sensor.
-   */
-  public boolean exampleCondition() {
-    // Query some boolean state, such as a digital sensor.
-    return false;
-  }
 
   @Override
   public void periodic() {
