a8257ffff3a9e35188c31086c423e60d1ee09e4b
Don Tran <donqt15@gmail.com>
2023-09-10 23:22:28 -0700

    claw motor setup

--- src/main/java/frc/robot/subsystems/ClawSubsystem.java
+++ src/main/java/frc/robot/subsystems/ClawSubsystem.java
@@ -1,7 +1,36 @@
 package frc.robot.subsystems;
 
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
 
 public class ClawSubsystem extends SubsystemBase {
-    public ClawSubsystem() {}
+    private final CANSparkMax clawMotor = new CANSparkMax(Constants.Claw.claw_motor_id, CANSparkMax.MotorType.kBrushless);
+    private final RelativeEncoder clawEncoder = clawMotor.getEncoder();
+    
+    public ClawSubsystem() {
+        clawMotor.restoreFactoryDefaults();
+
+        clawMotor.setInverted(true);
+
+        clawEncoder.setPositionConversionFactor(1.0 / 5.0); // TODO: check for value on this
+        clawEncoder.setVelocityConversionFactor(1.0 / (60.0 * 5.0)); // TODO: check for value on this
+
+
+        clawMotor.setSmartCurrentLimit(40); // TODO: check for optimal value on this
+    }
+
+    public void setClawMotor(double speed) {
+        clawMotor.set(speed);
+    }
+
+    public double getClawMotorSpeed() {
+        return clawMotor.get();
+    }
+
+    public void stop() {
+        clawMotor.stopMotor();
+    }
 }
