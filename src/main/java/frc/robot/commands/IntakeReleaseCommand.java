08e8a0f0c5c37b7b135c3df2360d47a2497fae86
Don Tran <donqt15@gmail.com>
2023-09-10 23:41:44 -0700

    refactored command name, deleted unused command

--- src/main/java/frc/robot/commands/IntakeReleaseCommand.java
+++ src/main/java/frc/robot/commands/IntakeReleaseCommand.java
@@ -3,23 +3,29 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.ClawSubsystem;
 
-public class Release extends CommandBase{
+public class IntakeReleaseCommand extends CommandBase{
     @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
     private final ClawSubsystem clawSubsystem;
+    private final double speed;
 
-    public Release (ClawSubsystem subsystem) {
-        clawSubsystem = subsystem;
+    public IntakeReleaseCommand(ClawSubsystem subsystem, double speed) {
+        this.clawSubsystem = subsystem;
+        this.speed = speed;
         addRequirements(clawSubsystem);
     }
 
     @Override
-    public void initialize() {}
+    public void initialize() {
+        clawSubsystem.setClawMotor(speed);
+    }
 
     @Override
     public void execute() {}
 
     @Override
-    public void end(boolean interrupted) {}
+    public void end(boolean interrupted) {
+        clawSubsystem.stop();
+    }
 
     @Override
     public boolean isFinished() {
