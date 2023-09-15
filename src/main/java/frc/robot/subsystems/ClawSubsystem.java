1adc4149a7cad567ead89c0d72e4923428a95c36
NoahSimon8 <noah.simon9806@gmail.com>
2023-09-14 20:34:15 -0700

    PID Control Example

--- src/main/java/frc/robot/subsystems/ClawSubsystem.java
+++ src/main/java/frc/robot/subsystems/ClawSubsystem.java
@@ -3,12 +3,14 @@ package frc.robot.subsystems;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.RelativeEncoder;
 
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 public class ClawSubsystem extends SubsystemBase {
     private final CANSparkMax clawMotor = new CANSparkMax(Constants.Claw.claw_motor_id, CANSparkMax.MotorType.kBrushless);
     private final RelativeEncoder clawEncoder = clawMotor.getEncoder();
+    private PIDController clawController = new PIDController(1, 0,0 );
     
     public ClawSubsystem() {
         clawMotor.restoreFactoryDefaults();
@@ -22,8 +24,13 @@ public class ClawSubsystem extends SubsystemBase {
         clawMotor.setSmartCurrentLimit(40); // TODO: check for optimal value on this
     }
 
+    // speed is rot/s
     public void setClawMotor(double speed) {
-        clawMotor.set(speed);
+
+        double voltage = clawController.calculate(clawEncoder.getVelocity(), speed);
+
+
+        clawMotor.set(voltage);
     }
 
     public double getClawMotorSpeed() {
