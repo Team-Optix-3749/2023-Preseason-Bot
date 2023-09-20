297ec42347e62fa7a6a5abf4b3fbf4ae7fb86cd3
Don Tran <donqt15@gmail.com>
2023-09-19 18:50:19 -0700

    reset wrist command

---
+++ src/main/java/frc/robot/commands/ResetWrist.java
@@ -0,0 +1,31 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.WristSubsystem;
+
+public class ResetWrist extends CommandBase {
+    @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
+    private final WristSubsystem wristSubsystem;
+
+    public ResetWrist(WristSubsystem subsystem, double angle) {
+        this.wristSubsystem = subsystem;
+        addRequirements(wristSubsystem);
+    }
+
+    @Override
+    public void initialize() {}
+
+    @Override
+    public void execute() { // check if this works
+        wristSubsystem.resetWristAngle();
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+}
+
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+}
